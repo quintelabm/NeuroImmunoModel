606ee5694345b01e44db366d9ec694037279e08a
Matheus-Avila <matheusavila1997@gmail.com>
2022-09-14 21:37:18 -0300

    random vessels nao esta influenciando

--- neuroimmunomodel.py
+++ neuroimmunomodel.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import random as rnd
 import time
 import os
 from scipy.integrate import odeint
@@ -48,20 +49,19 @@ V_BV = 0
 V_LV = 0
 
 theta_LV = np.zeros((int(L/h_x), int(L/h_x)))
-for i in range(int(L/h_x)):
-    for j in range(int(L/h_x)):
-        # if (i == L/h_x - 1 and j == L/(h_x*2)) or (i == 0 and j == L/(h_x*2)) or (i == L/(h_x*2) and j == 0) or (i == L/(h_x*2) and j == L/h_x - 1) or (i == int(L/h_x)/2 and j == int(L/h_x)/2):
-        if (j == 0) or (i == int(L/h_x)/2 and j == int(L/h_x)/2):
-            theta_LV[i][j] = 1
-            V_LV += 1
-
 theta_BV = np.zeros((int(L/h_x), int(L/h_x)))
 for i in range(int(L/h_x)):
     for j in range(int(L/h_x)):
-        # if (i == L/h_x - 1 and j == L/h_x - 1) or (i == 0 and j == L/h_x - 1) or (i == L/h_x - 1 and j == 0) or (i == 0 and j == 0):
-        if j == int(L/h_x) -1:
-            theta_BV[i][j] = 1
-            V_BV += 1
+        prob = rnd.uniform(0,1)
+        if prob <= .1:
+            theta_LV[i][j] = 1
+            V_LV += 1
+            if j == int(L/h_x) -1:
+                theta_BV[i][0] = 1
+                V_BV += 1
+            else:
+                theta_BV[i][j+1] = 1
+                V_BV += 1
 
 
 V_LN = 160
